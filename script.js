c68da4a062d5712cf8be40741691fdc2e991c766
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-06 22:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+const panels=document.querySelectorAll('.panel')
+	panels.forEach((panel)=>{
+		panel.addEventListener('click',()=>{
+			panels.forEach(data=>{
+				data.classList.remove('active');
+			})
+			panel.classList.add('active');
+		})
+	})
+})
